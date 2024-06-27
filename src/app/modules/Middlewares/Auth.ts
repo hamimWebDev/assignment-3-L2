747c141c995442227f7d95fcd980d11885cafe61
hamimDev <mdhamimhowladerasif@gmail.com>
2024-06-27 19:47:06 +0600

    update auth

--- src/app/modules/Middlewares/Auth.ts
+++ src/app/modules/Middlewares/Auth.ts
@@ -1,14 +1,13 @@
 import httpStatus from "http-status";
-import { AppError } from "../errors/AppErrors";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
 import { TUserRole } from "../User/UserInterface";
 import { catchAsync } from "../Utils/CatchAsync";
+import { AppError } from "../errors/AppErrors";
 import config from "../../config";
 import { User } from "../User/UserSchemaModel";
 
 export const auth = (...requiredRoles: TUserRole[]) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  return catchAsync(async (req, res, next) => {
     const token = req.headers.authorization;
     if (!token) {
       throw new AppError(
@@ -24,9 +23,9 @@ export const auth = (...requiredRoles: TUserRole[]) => {
     req.user = decoded;
 
     // Example usage of 'role'
-    const { role, userId } = decoded;
+    const { role, _id } = decoded;
 
-    const user = await User?.isUserExistByCustomId(userId);
+    const user = await User?.isUserExistByCustomId(_id);
     if (!user) {
       throw new AppError(httpStatus.NOT_FOUND, "user is fot found");
     }
